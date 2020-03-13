d9faa065759076cd3297236e56d46ac9836619e1
umutZeren <60076817+umutZeren@users.noreply.github.com>
2020-03-13 13:33:58 +0300

    a comment and if check gone

--- fileManege.rb
+++ fileManege.rb
@@ -1,12 +1,11 @@
 #puts file.readline()
 #file.read().include? "bla bla" puts cursor to the end
 #w+ overrites
-# do blocku eksik olmasın
+
 to_write=Array.new
 File.open("C:\\Users\\Casper\\Desktop\\yeni klasör\\modules_try.rb","r") do |file|
 for i in file.readlines()
 if(i[0]=="#")
-	puts "ifde"
 i=""
 else
 end
